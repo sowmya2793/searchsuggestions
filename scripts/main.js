72ece19e48575e1a4272105bbbcfd1db1bb714d4
Sowmya kini <sowmyaskini@gmail.com>
2021-11-17 10:26:41 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -2,15 +2,18 @@
 let url = 'https://spark.innosabi.com/api/v4/suggestion';
 const username = 'api@innosabi.com';
         const password = '0thRuch0';
-        let headers = new Headers();
+        /*let headers = new Headers();
 headers.set('Authorization', 'Basic ' + btoa(username + ":" + password));
 headers.append('Content-Type', 'application/json');
-headers.append('Accept', 'application/json');
+headers.append('Accept', 'application/json');*/
 fetch(url, {
         method:'GET',
         mode: 'cors',
-        headers: headers,
-        credentials: 'omit'
+        headers: {
+                Authorization :  'Basic ' + btoa('username: password'),
+                Content-Type : 'application/json'
+        },
+        //credentials: 'omit'
        })
 .then(data=>  {
         console.log(data);
