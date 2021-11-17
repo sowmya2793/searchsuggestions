3114519049d0cee810eed1206be0ac6a5cf4a870
Sowmya kini <sowmyaskini@gmail.com>
2021-11-17 08:59:27 +0100

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,57 @@
+//work around for Cors- does not allow Localhost or local file to fetch from https
+var cors_api_url = 'https://cors-anywhere.herokuapp.com/';
+function doCORSRequest(options, printResult) {
+        var x = new XMLHttpRequest();
+        x.open(options.method, cors_api_url + options.url);
+        x.onload = x.onerror = function () {
+                printResult(
+                        options.method + ' ' + options.url + '\n' +
+                        x.status + ' ' + x.statusText + '\n\n' +
+                        (x.responseText || '')
+                );
+        };
+        if (/^POST/i.test(options.method)) {
+                x.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        }
+        x.send(options.data);
+}
+
+// Bind event
+(function () {
+        let url = 'https://spark.innosabi.com/api/v4/suggestion';
+        const username = 'api@innosabi.com';
+        const password = '0thRuch0';
+        let headers = new Headers();
+        headers.set('Authorization', 'Basic ' + btoa(username + ":" + password));
+        headers.append('Content-Type', 'application/json');
+        var outputresult = document.getElementsByClassName('search-result');
+        doCORSRequest({
+                method: 'GET',
+                url: url,
+                headers: headers,
+                data: ''
+        }, function printResult(result) {
+                outputresult.value = result;
+        });
+})();
+
+//init TBD instead of code above
+/*let url = 'https://spark.innosabi.com/api/v4/suggestion';
+headers.set('Authorization', 'Basic ' + btoa(username + ":" + password));
+headers.append('Content-Type', 'application/json');
+headers.append('Accept', 'application/json');
+fetch(url, {
+        method:'GET',
+        mode: 'no-cors',
+        credentials: 'omit',
+        headers: headers,
+       })
+.then(data=>  {
+        console.log(data);
+    })
+.catch(error => {
+  console.log('Error:', error);
+}); */
+/* let params = { "params": "innovation" };
+        let queryterm = Object.keys(params).map(k => encodeURIComponent(k) + '=' + encodeURIComponent(params[k])).join('&');
+        */
